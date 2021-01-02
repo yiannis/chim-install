cfebdfd80ee0fe101b0d0699c36e6a0cb44c19e3
Yiannis Belias <yiannisbe@gmail.com>
2021-01-02 12:13:31 +0200

    More stage 8 actions

--- scripts/01-bootstrap-chroot.sh
+++ scripts/01-bootstrap-chroot.sh
@@ -3,3 +3,4 @@ sudo debootstrap --variant=minbase bionic /mnt/data/chimere-chroot http://archiv
 cat /etc/apt/sources.list | grep -vi docker | sudo tee /mnt/data/chimere-chroot/etc/apt/sources.list > /dev/null
 
 sudo cp files/nc4internal.h /mnt/data/chimere-chroot/root/
+sudo cp files/mychimere-ubuntu-18.04 /mnt/data/chimere-chroot/root/
--- scripts/08-inside-chroot-chimere.sh
+++ scripts/08-inside-chroot-chimere.sh
@@ -3,7 +3,7 @@ mkdir -p ~/incoming/chimere
 
 cd ~/incoming/chimere
 for tgz in {chimere_v2020r1.tar.gz,emiSURF2020.tar.gz,BIGFILES2020.tar.gz,Emisurf_domains.tar.gz,MEGAN_30s.tar.gz,TestCase2020.tar.gz,TestCase3Nests2020.tar.gz,TestCaseEMISURF2020.tar.gz,emiFIRES_CAMS_v2020r1.tar.gz}; do
-  if -f ${tgz}; then
+  if [ -f ${tgz} ]; then
     echo File ${tgz} exists. Skipping...
   else
     wget --user 'ichristo@phys.uoa.gr' --password='ichristo.2018' ${tgz}
@@ -12,11 +12,17 @@ done
 
 cd /CHIMERE
 for tgz in {chimere_v2020r1.tar.gz,emiSURF2020.tar.gz,BIGFILES2020.tar.gz,Emisurf_domains.tar.gz,MEGAN_30s.tar.gz,TestCase2020.tar.gz,TestCase3Nests2020.tar.gz,TestCaseEMISURF2020.tar.gz,emiFIRES_CAMS_v2020r1.tar.gz}; do
+  echo Extracting: ${tgz}
   tar xf ~/incoming/chimere/${tgz}
 done
 
 apt-get install mlocate gawk
 
 updatedb # config.sh uses locate to find paths
+
+cd /CHIMERE/chimere_v2020r1/mychimere/makefiles.hdr
+ln -s Makefile.hdr.gfortran-64-ompi Makefile.hdr.gfortran-4.8-64-ompi
+
+cp /root/mychimere-ubuntu-18.04 /CHIMERE/chimere_v2020r1/mychimere/
 
 
